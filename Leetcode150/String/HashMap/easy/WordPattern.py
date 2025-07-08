ccb27c34c38024933e96cd3dfc2858ebd02cf4b6
AmitHeelDev <amit@theheel.com>
2025-07-07 22:39:55 -0500

    - Solved LC: 290. Word Pattern

---
+++ Leetcode150/String/HashMap/easy/WordPattern.py
@@ -0,0 +1,77 @@
+"""
+Given a pattern and a string s, find if s follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s. Specifically:
+
+Each letter in pattern maps to exactly one unique word in s.
+Each unique word in s maps to exactly one letter in pattern.
+No two letters map to the same word, and no two words map to the same letter.
+
+
+Example 1:
+
+Input: pattern = "abba", s = "dog cat cat dog"
+
+Output: true
+
+Explanation:
+
+The bijection can be established as:
+
+'a' maps to "dog".
+'b' maps to "cat".
+Example 2:
+
+Input: pattern = "abba", s = "dog cat cat fish"
+
+Output: false
+
+Example 3:
+
+Input: pattern = "aaaa", s = "dog cat cat dog"
+
+Output: false
+
+
+
+Constraints:
+
+1 <= pattern.length <= 300
+pattern contains only lower-case English letters.
+1 <= s.length <= 3000
+s contains only lowercase English letters and spaces ' '.
+s does not contain any leading or trailing spaces.
+All the words in s are separated by a single space.
+"""
+
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s_dict: dict = {}
+        t_dict : dict = {}
+        i: int = 0
+        s_arr = s.split(" ")
+        if len(pattern) != len(s_arr): return False
+        while i < len(pattern):
+            if pattern[i] in s_dict:
+                if s_dict[pattern[i]] != s_arr[i]:
+                    return False
+            elif s_arr[i] in t_dict:
+                if t_dict[s_arr[i]]!= pattern[i]:
+                    return False
+            else:
+                s_dict[pattern[i]] = s_arr[i]
+                t_dict[s_arr[i]] = pattern[i]
+            i += 1
+
+        return True
+
+    def main(self):
+        print(self.wordPattern(pattern="abba", s="dog cat cat dog"))
+        print(self.wordPattern(pattern="abba", s="dog cat cat fish"))
+        print(self.wordPattern(pattern="aaaa", s="dog cat cat dog"))
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    sol.main()
